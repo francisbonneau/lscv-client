81beb7e73ea889dad3daa7ebf0545d4e5e4f1446
Francis Bonneau <francis.bonneau@gmail.com>
2014-08-20 22:59:43 -0400

    added data processing

--- src/Data/DataAggregator.java
+++ src/Data/DataAggregator.java
@@ -1,15 +1,20 @@
 package data;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Queue;
 
 import processing.RenderLoop;
 
 public class DataAggregator implements Observer {
 	
 	private RenderLoop renderLoop;
+	
+	public Queue<HashMap<String, HashMap<Integer, Integer>>> data;
 	
 	public DataAggregator(RenderLoop renderLoop) {
 		
@@ -19,25 +24,33 @@ public class DataAggregator implements Observer {
 			System.out.println("starting redis thread for host " + host);
 			Thread t = new Thread(new RedisDataSource(host, this));
 	        t.start();
+	        
+	        // TODO create a queue for each host to put data
 		}
+		
+		data = new LinkedList<HashMap<String, HashMap<Integer, Integer>>>();		
 	}
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		
+ 
 		RedisDataSource source = (RedisDataSource) arg0;
 		System.out.println("update from " + source.ip + " received");
 				
+		@SuppressWarnings("unchecked")
 		Map<String, String> data = (Map<String, String>) arg1;
-		processData(data);
-		
+		processData(data);		
 	}
 	
-	public void processData(Map<String, String> data) {
+	public void processData(Map<String, String> dataToProcess) {
 		
-		Iterator<String> it = data.values().iterator();
-		Iterator<String> it2 = data.keySet().iterator();
+		float roundup = renderLoop.params.latencyRoundup;
+		
+		Iterator<String> it = dataToProcess.keySet().iterator();
+		Iterator<String> it2 = dataToProcess.values().iterator();
+		
+		HashMap<String, HashMap<Integer, Integer>> processedData = new
+			HashMap<String, HashMap<Integer, Integer>>();
 		
 		while(it.hasNext()) { 
 			
@@ -48,15 +61,28 @@ public class DataAggregator implements Observer {
 			String key = it.next();
 			String value = it2.next();
 			
+			// breakdown event latency by process								
+			String processName = key.split("\\.")[1];
 			
+			HashMap<Integer, Integer> latencyBreakdown = new HashMap<Integer, Integer>();
 			
-		
-			data.put(key, value);
+			for (String val : value.split(",")) {								
+				Double exp = Math.pow(10, 9 - roundup);				
+				Double tmp = Math.floor(Double.parseDouble(val) / exp);								
+				int roundedVal = (int) (tmp * exp);
+								
+				if (latencyBreakdown.get(roundedVal) == null) {
+					latencyBreakdown.put(roundedVal, 1);
+				} else {
+					int newVal = latencyBreakdown.get(roundedVal) + 1;
+					latencyBreakdown.put(roundedVal, newVal);
+				}
+			}						
 			
+			processedData.put(processName, latencyBreakdown);			
 		}
 		
-		
-		
+		data.add(processedData);
 	}
 
 }
--- src/processing/Params.java
+++ src/processing/Params.java
@@ -19,15 +19,22 @@ public class Params {
 	
 	// between 1 and 8, used to control the number of particles
 	// displayed by rounding up the events lat. in buckets (heat map)
+	// 10^−1 s	100 ms millisecond
+	// 10^−2 s	10 	ms millisecond
+	// 10^−3 s	1   ms millisecond
+	// 10^−4 s	100 µs microsecond
+	// 10^−5 s	10  µs microsecond
+	// 10^−6 s	1   µs microsecond
+	// 10^−7 s	100 ns nanosecond
+	// 10^−8 s	10  ns nanosecond
+	// 10^−9 s	1   ns nanosecond
+	public float latencyRoundup = 6;
 	
-	// 10^−1 s	ds	decisecond
-	// 10^−2 s	cs	centisecond
-	// 10^−3 s	ms	millisecond
-	// 10^−6 s	µs	microsecond
-	// 10^−9 s	ns	nanosecond
-	public float latencyRoundup = 2; 
-	
-	
+	public String[] latencyRoundupLegend = {
+		"100 ms", "10 ms", "1 ms",
+		"100 µs", "10 µs", "1 µs",
+		"100 ns", "10 ns", "1 ns"
+	};
 	
 	
 }
