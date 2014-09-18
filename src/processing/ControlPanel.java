4b9c08c97ae7c6972d3d593b026b2f353a3ca9de
Francis Bonneau <francis.bonneau@gmail.com>
2014-09-18 16:27:59 -0400

    added multiples emitters UI sliders

--- src/processing/ControlPanel.java
+++ src/processing/ControlPanel.java
@@ -4,6 +4,7 @@ import processing.core.PApplet;
 import processing.core.PFont;
 import controlP5.ControlEvent;
 import controlP5.ControlP5;
+import controlP5.Slider;
 import controlP5.Textlabel;
 
 //the ControlFrame class extends PApplet, so we
@@ -34,31 +35,50 @@ public class ControlPanel extends PApplet {
 		PFont openSans15 = loadFont("OpenSans-15.vlw");
 		cp5.setFont(openSans14);
 		
-		cp5.addTextlabel("label1", "GLOBAL SETTINGS",20,20);
+		cp5.addTextlabel("label1", "GLOBAL SETTINGS", 20, 20);
+
+		cp5.addSlider("Background brightness", 0, 100, 30, 50, 500, 15)
+				.setId(1).setValue(rl.params.backgroundBrightness);
+
+		cp5.addSlider("Emitter radius", 100, 2000, 30, 80, 500, 15).setId(2)
+				.setValue(rl.params.emitterRadius);
+
+		cp5.addSlider("Radius brightness", 0, 100, 30, 110, 500, 15).setId(3)
+				.setValue(rl.params.emitterRadiusBrightness);
+
+		cp5.addSlider("Particle size", 1, 400, 30, 140, 500, 15).setId(4)
+				.setValue(rl.params.particleSize);
+
+		cp5.addRange("Particle velocity", rl.params.particleMinVelocity,
+				rl.params.particleMaxVelocity,
+				rl.params.particleCurrentMinVelocity,
+				rl.params.particleCurrentMaxVelocity, 30, 170, 500, 15)
+				.setId(5);
+
+		cp5.addSlider("Particle acceleration", 0, 1, 30, 200, 500, 15).setId(6)
+				.setValue(rl.params.particleAcceleration);
+
+		cp5.addSlider("Latency rounding", 1, 8, 30, 230, 500, 15).setId(7)
+				.setValue(rl.params.latencyRoundup).setNumberOfTickMarks(8)
+				.showTickMarks(false);
 		
-		cp5.addSlider("Background brightness", 0, 100, 30, 50, 500, 15).setId(1).setValue(rl.params.backgroundBrightness);
+		cp5.addTextlabel("label2", "( "
+						+ rl.params.latencyRoundupLegend[rl.params.latencyRoundup - 1]
+						+ " )", 680, 230).setFont(openSans15);
 		
-		cp5.addSlider("Emitter radius", 100, 2000, 30, 80, 500, 15).setId(2).setValue(rl.params.emitterRadius);
+		cp5.addTextlabel("label3", "DATA SOURCES", 20, 275);
 		
-		cp5.addSlider("Radius brightness", 0, 100, 30, 110, 500, 15).setId(3).setValue(rl.params.emitterRadiusBrightness);
+		cp5.addSlider("Number of Emitters", 1, 8, 1, 80, 305, 450, 10).setId(8)
+				.setNumberOfTickMarks(8).showTickMarks(true)
+				.setSliderMode(Slider.FLEXIBLE).valueLabel().setVisible(false);		
+
+		cp5.addSlider(" ", 1, 4, 1, 40, 335, 10, 150).setId(9)
+				.setNumberOfTickMarks(4).showTickMarks(true)
+				.setSliderMode(Slider.FLEXIBLE).valueLabel().setVisible(false);
 		
-		cp5.addSlider("Particle size", 1, 400, 30, 140, 500, 15).setId(4).setValue(rl.params.particleSize);
 		
 		
-		cp5.addRange("Particle velocity", rl.params.particleMinVelocity, rl.params.particleMaxVelocity,
-				rl.params.particleCurrentMinVelocity, rl.params.particleCurrentMaxVelocity, 30, 170, 500, 15).setId(5);
 		
-		//cp5.addSlider("Particle Max velocity", 1, 10000, 30, 150, 500, 15).setId(5).setValue(rl.params.particleMaxVelocity);
-		
-		cp5.addSlider("Particle acceleration", 0, 1, 30, 200, 500, 15).setId(6).setValue(rl.params.particleAcceleration);
-		
-		cp5.addSlider("Latency rounding", 1, 8, 30, 230, 500, 15).setId(7).setValue(rl.params.latencyRoundup).setNumberOfTickMarks(8).showTickMarks(true);
-		
-//		cp5.addTextlabel("label2", "Current event latency precision : " + 
-//				rl.params.latencyRoundupLegend[rl.params.latencyRoundup],25,245);
-		
-		cp5.addTextlabel("label2", "( " + rl.params.latencyRoundupLegend[rl.params.latencyRoundup - 1] + " )",
-				680, 230).setFont(openSans15);
 	}
 	
 	public void controlEvent(ControlEvent theEvent) {
