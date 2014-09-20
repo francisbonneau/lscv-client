bdd8bae1abe3b1b84e35e89a80a186f9d8432766
Francis Bonneau <francis.bonneau@gmail.com>
2014-09-20 00:03:49 -0400

    test new default width

--- src/processing/Params.java
+++ src/processing/Params.java
@@ -8,7 +8,7 @@ public class Params {
 	// display parameters
 	public boolean fullscreen = false;
 	public int defaultHeight = 800;
-	public int defaultWidth = 800;
+	public int defaultWidth = 1200;
 	public int framerate = 30;	
 	public boolean resizable = false;
 	
