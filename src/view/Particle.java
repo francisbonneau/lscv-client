d1925f8d84b6be6199953a02b14fe238b5005df0
Francis Bonneau <francis.bonneau@gmail.com>
2014-09-27 19:00:05 -0400

    little refactoring of emitter/particle

--- src/view/Particle.java
+++ src/view/Particle.java
@@ -1,48 +1,78 @@
 package view;
 import model.Event;
-import model.Params;
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PVector;
 
 public class Particle {
 	
+	// the PApplet where the particle is drew
 	private PApplet p;
 	
-	// particle data
+	// the data that this particle represent
 	public Event event;
 	
-	// particle properties
+	// the position X,Y
 	public PVector location;
+	
+	// the particle angle (in degrees) from the Emitter center
+	private float angle;
+	
+	// the particle speed
 	public PVector velocity;
-	public PVector acceleration;	
-	public float color;
+	
+	// the particle acceleration
+	public PVector acceleration;
+	
+	// the particle hue (H) in the HSB model
+	private float hue;
+	
+	// the particle brightness (B) in the HSB model
 	public float brightness;
+	
+	// the particle size in pixels
 	public float size;
 	
+	/**
+	 * Create a new particle to be displayed in the PApplet p
+	 * @param p	the PApplet where the particle is displayed
+	 * @param event the data associated to the particle
+	 */
 	public Particle(PApplet p, Event event) {
 		this.p = p;		
-		this.event = event;
+		this.event = event;	
+	}
+	
+	// Configure the particle according to the parameters in argument
+	public void setup(PVector location, float size, float angle, 
+			float baseVelocity, float baseAcceleration, float hue, 
+			float brightness) {
 		
-		this.brightness = 100;
-	}
-	
-	public void setup(PVector location, Params params) {				
+		this.size = size;
 		this.location = location.get();
-		velocity = new PVector(params.particleMaxVelocity, params.particleMaxVelocity);
-		acceleration = new PVector(params.particleAcceleration, params.particleAcceleration);
-		size = params.particleSize;
+		this.angle = angle;
+		
+		this.velocity = new PVector(baseVelocity, baseVelocity);		
+		this.acceleration = new PVector(baseAcceleration, baseAcceleration);
+		
+		this.velocity.rotate(PApplet.radians(this.angle));
+		this.acceleration.rotate(PApplet.radians(this.angle));		
+		
+		this.hue = hue;
+		this.brightness = brightness;
 	}
-	
+
+	// Update the particle position and speed (by one unit of time)
 	public void update() { 
 		velocity.add(acceleration);
 		location.add(velocity);		
 	}
 	
+	// Draw the particle on the PApplet
 	public void draw() {
 		p.stroke(0);
 		p.colorMode(PConstants.HSB, 360, 100, 100);
-		p.fill(color, 100, brightness);
+		p.fill(hue, 100, brightness);
 		p.ellipse(location.x, location.y, size, size);
 	}
 	
