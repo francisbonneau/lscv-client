7c4ee8b56cff30065df4adff601c3328e344fbaf
Francis Bonneau <francis.bonneau@gmail.com>
2014-11-30 12:50:40 -0500

    Labels repositioned when the halos are on/off

--- src/view/EmitterLabel.java
+++ src/view/EmitterLabel.java
@@ -6,30 +6,36 @@ import processing.core.PConstants;
 public class EmitterLabel {
 
     private PApplet p;
-
-    public String text;
+    public String divisionID;
     public Integer textSize;
     public float color;
     public float positionX;
     public float positionY;
 
-    public EmitterLabel(PApplet p, String text, Integer textSize,
-            float color,float positionX, float positionY) {
+    public EmitterLabel(PApplet p, String text, Integer textSize, float color) {
 
         this.p = p;
-        this.text = text;
+        this.divisionID = text;
         this.textSize = textSize;
         this.color = color;
-        this.positionX = positionX;
-        this.positionY = positionY;
     }
 
+    public void calculateLabelPosition(float minAngle, float maxAngle,
+    		float emitterRadius, float emitterCenterX, float emitterCenterY,
+    		float labelsDistance) {
+
+        float angle = (minAngle + maxAngle)/2 + 45;
+        float radius = emitterRadius/2 + labelsDistance;
+
+        positionX = (float) Math.cos(PApplet.radians(angle)) * radius + emitterCenterX;
+        positionY = (float) Math.sin(PApplet.radians(angle)) * radius + emitterCenterY;
+    }
 
     public void draw() {
         p.colorMode(PConstants.HSB, 360, 100, 100);
         p.textSize(textSize);
         p.fill(this.color, 100, 100);
-        p.text(text, positionX, positionY);
+        p.text(divisionID, positionX, positionY);
 
     }
 
