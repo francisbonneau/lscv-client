30066be2aff5d83a0b4d04865d27b2566ce9f4f7
Francis Bonneau <francis.bonneau@gmail.com>
2014-12-03 14:11:12 -0500

    updated the help menu slides

--- src/view/HelpMenu.java
+++ src/view/HelpMenu.java
@@ -20,17 +20,20 @@ public class HelpMenu {
 		slides = new ArrayList<>();
 
 		// Load the help menu slides images
-		PImage slide1 = p.loadImage("help1.jpg");
+		PImage slide1 = p.loadImage("slide1.png");
 		slides.add(slide1);
 
-		PImage slide2 = p.loadImage("help2.jpg");
+		PImage slide2 = p.loadImage("slide2.png");
 		slides.add(slide2);
 
-		PImage slide3 = p.loadImage("help3.jpg");
+		PImage slide3 = p.loadImage("slide3.png");
 		slides.add(slide3);
 
-		PImage slide4 = p.loadImage("help4.jpg");
+		PImage slide4 = p.loadImage("slide4.png");
 		slides.add(slide4);
+
+		PImage slide5 = p.loadImage("slide5.png");
+		slides.add(slide5);
 
 		currentSlide = slide1;
 	}
@@ -46,8 +49,6 @@ public class HelpMenu {
 
 		if (index != slides.size() - 1) {
 			index++;
-		} else {
-			index = 0;
 		}
 		currentSlide = slides.get(index);
 	}
@@ -58,8 +59,6 @@ public class HelpMenu {
 
 		if (index != 0) {
 			index--;
-		} else {
-			index = slides.size() - 1;
 		}
 		currentSlide = slides.get(index);
 	}
